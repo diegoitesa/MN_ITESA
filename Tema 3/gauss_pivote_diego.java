133c308e179b18b499a539749d55343811f95bae
diegoitesa <23030680@itesa.edu.mx>
2025-03-10 08:59:39 -0600

    Avance de la función gaussPivote

--- Tema 3/gauss_pivote_diego.java
+++ Tema 3/gauss_pivote_diego.java
@@ -5,11 +5,13 @@ public class gauss_pivote
 {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
-        System.err.print("Tamaño de matriz: ");
+        System.err.print("Tamanio de matriz: ");
         int x = read.nextInt();
         double[][] matriz = new double[x][x];
 
         llenarMatriz(matriz, x);
+        imprimirMatriz(matriz);
+        gaussPivote(matriz);
         imprimirMatriz(matriz);
     }
 
@@ -37,7 +39,30 @@ public class gauss_pivote
 
     public static void gaussPivote(double[][] m)
     {
-        
+        // inicializar la matriz en la primer esquina
+        int pivP = 0;
+        double pivote = m[pivP][pivP];
+
+        while ( pivP != m[0].length)
+        {
+            pivote = m[pivP][pivP];
+            
+            // convertimos el pivote en 1
+            for (int i = 0; i < m[0].length; i++)
+                m[pivP][i] /= pivote;
+            
+            
+
+            for (int i = pivP; i <= m[0].length; i++){
+                pivote = m[pivP][pivP] /= pivote;
+                for (int j = pivP + 1; j <= m[0].length; j++)
+                    m[j][i] += (-m[j][i]) * pivote;
+            }
+                
+            
+            pivP++;
+        }
+            
     }
 
 
