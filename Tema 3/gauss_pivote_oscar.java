0d8e61fbb1e135cdf405f8e0997da85e3c51b070
Oscar Aaron <23030042@itesa.edu.mx>
2025-03-12 19:53:55 -0600

    Implementación del metodo de gaussPivote para realizar la solución.

--- Tema 3/gauss_pivote_oscar.java
+++ Tema 3/gauss_pivote_oscar.java
@@ -9,7 +9,13 @@ public class gauss_pivote_oscar {
         System.out.println("Introduce el tamaño de la matriz: ");
         int x = scanner.nextInt();
         double matriz[][] = new double[x][x]; 
+
         llenado(matriz);
+        System.out.println("Matriz original:");
+        mostrar(matriz);
+
+        gaussPivote(matriz);
+        System.out.println("Matriz despues del gaussPivote:");
         mostrar(matriz);
     }
 
@@ -31,6 +37,21 @@ public class gauss_pivote_oscar {
                 System.out.print("[ " + String.format("%.2f", m[i][j]) + "]");
             }
             System.out.println();
+        }
+    }
+
+    public static void gaussPivote(double[][] m) {
+        int n = m.length;
+        for(int pivote = 0; pivote < n; pivote++){
+            double factor = m[pivote][pivote];
+            for(int j = 0; j < n; j++)
+                m[pivote][j] /= factor;
+
+            for(int i = pivote + 1; i < n; i++){
+                double coef = m[i][pivote];
+                for(int j = 0; j < n; j++)
+                    m[i][j] -= coef * m[pivote][j];
+            }
         }
     }
 }
