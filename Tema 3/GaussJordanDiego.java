4d14235dd3a7cb04c089ff318ee877c7914df9e8
diegoitesa <23030680@itesa.edu.mx>
2025-03-13 08:54:14 -0600

    Versión 4. Documentación del código y optimización del mismo.

--- Tema 3/GaussJordanDiego.java
+++ Tema 3/GaussJordanDiego.java
@@ -1,77 +1,97 @@
 import java.util.Scanner;
+import java.util.Random;
 
-/*
-*   Programador: Diego Alonso Coronel Vargas
-*   
-*   Fecha: 13/03/2025
-*   
-*   Descripción: El programa aplica el algoritmo de Gauss-Jordan  
-*   a una matriz que es generada automáticamente dependiendo del   
-*   tamaño que introduzca el usuario.
-*
-*/
+/**
+ * Aplicación que implementa el algoritmo de eliminación de Gauss-Jordan.
+ * La matriz se genera automáticamente con valores aleatorios en función del tamaño ingresado por el usuario.
+ * 
+ * @author Diego Alonso Coronel Vargas
+ * @version 4.0
+ * @since 2025-03-13
+ */
 
 public class GaussJordanDiego
 {
     public static void main(String[] args)
     {
         Scanner read = new Scanner(System.in);
-        System.err.print("Tamanio de matriz: ");
-        int x = read.nextInt();
-        double[][] matriz = new double[x][x];
 
-        llenarMatriz(matriz, x);
-        imprimirMatriz(matriz);
-        gaussJordan(matriz);
-        System.err.println("");
-        imprimirMatriz(matriz);
+        System.err.print("Tamaño de la matriz: ");
+        int size = read.nextInt();
+
+        double[][] matrix = new double[size][size];
+
+        llenarMatriz(matrix, size);
+        System.out.println("Matriz original:");
+        imprimirMatriz(matrix);
+
+        gaussJordan(matrix);
+
+        System.out.println("\nMatriz después de aplicar Gauss Jordan:");
+        imprimirMatriz(matrix);
     }
 
-    private static void llenarMatriz(double[][] m, int lon)
+    /**
+     * Llena una matriz con valores aleatorios entre 0 y 20.
+     * 
+     * @param matrix Matriz a llenar.
+     * @param size Tamaño de la matriz (size x size).
+     */
+    private static void llenarMatriz(double[][] matrix, int size)
     {   
         Random random = new Random();
 
-        for (int i = 0; i < lon; i++)
-            for (int j = 0; j < lon; j++)
-                m[i][j] = random.nextDouble(20);
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                matrix[i][j] = random.nextDouble(20);
     }
 
-    private static void imprimirMatriz(double[][] m)
+    /**
+     * Imprime una matriz en formato legible.
+     * 
+     * @param matrix Matriz a imprimir.
+     */
+    private static void imprimirMatriz(double[][] matrix)
     {
-        int x = m[0].length;
-        for (int i = 0; i < x; i++){
-            System.err.println("");
-            for (int j = 0; j < x; j++)
-                System.out.printf("[%2.1f] ", m[i][j]);
+        int size = matrix[0].length;
+
+        for (int i = 0; i < size; i++){
+            System.err.println();
+            for (int j = 0; j < size; j++)
+                System.out.printf("[%5.2f] ", matrix[i][j]);
         }
+        System.out.println();
                  
     }
 
-    private static void gaussJordan(double[][] m)
+    /**
+     * Aplica el método de eliminación de Gauss-Jordan a la matriz.
+     * 
+     * @param matrix Matriz a transformar en su forma escalonada reducida.
+     */
+    private static void gaussJordan(double[][] matrix)
     {
-        // inicializar la matriz en la primer esquina
-        int pivP = 0;
+        int size = matrix.length;
 
-        // Ciclo para cubrir todos los pivotes de la matriz
-        while ( pivP < m[0].length)
-        {
-            // convertimos el pivote en 1
-            for (int i = 0; i < m[0].length; i++){
-                m[pivP][i] /= m[pivP][pivP];
-            }  
-            
-            // convertimos las filas que están sobre y debajo del pivote en 0
-            for (int i = 0; i < m[0].length; i++){
-                
-                // Saltamos la fila donde se encuentra el pivote para no afectarla
-                if (i == pivP)
-                    continue;
+        for (int pivotRow = 0; pivotRow < size; pivotRow++) {
+            double pivot = matrix[pivotRow][pivotRow];
 
-                for (int j = 0; j < m[0].length; j++)
-                    m[i][j] += (-m[i][j]) * m[pivP][j];
+            // Normalizar la fila del pivote (convertir pivote en 1)
+            for (int j = 0; j < size; j++) {
+                matrix[pivotRow][j] /= pivot;
             }
-                
-            pivP++;
+
+            // Convertir en ceros las demás filas en la columna del pivote
+            for (int i = 0; i < size; i++) {
+                if (i == pivotRow) {
+                    continue; // Saltar la fila del pivote
+                }
+
+                double factor = matrix[i][pivotRow]; // Factor de eliminación
+                for (int j = 0; j < size; j++) {
+                    matrix[i][j] -= factor * matrix[pivotRow][j];
+                }
+            }
         }
     }
 }
