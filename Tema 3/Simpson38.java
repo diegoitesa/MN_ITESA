fb6d8d90d16f371590dee6a60f9a15ccfa2ac2c5
diegoitesa <23030680@itesa.edu.mx>
2025-04-07 11:38:57 -0600

    Códio en Java de la Regla de Simpson 3/8

---
+++ Tema 3/Simpson38.java
@@ -0,0 +1,26 @@
+public class Simpson38 {
+
+    // f(x):
+    public static double f(double x) {
+        return Math.pow(x, 3) + 2 * Math.pow(x, 2);
+    }
+
+    public static double simpson38(double a, double b) {
+        double h = (b - a) / 3.0;
+        double x0 = a;
+        double x1 = a + h;
+        double x2 = a + 2 * h;
+        double x3 = b;
+
+        double result = (3 * h / 8.0) * (f(x0) + 3 * f(x1) + 3 * f(x2) + f(x3));
+        return result;
+    }
+
+    public static void main(String[] args) {
+        double a = 0;  // Límite inferior
+        double b = 3;  // Límite superior
+
+        double integral = simpson38(a, b);
+        System.out.printf("Resultado aproximado: %.6f%n", integral);
+    }
+}
