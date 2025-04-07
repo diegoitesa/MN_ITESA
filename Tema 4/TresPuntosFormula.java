53ad5d405b0826cf5ab34fd42b02d1cfe265452e
diegoitesa <23030680@itesa.edu.mx>
2025-04-07 12:48:15 -0600

    Función para la fórmula de los tres puntos

--- Tema 4/TresPuntosFormula.java
+++ Tema 4/TresPuntosFormula.java
@@ -1,8 +1,18 @@
 public class TresPuntosFormula {
 
-    // f(x):
+    // f(x)
     public static double f(double x) {
-        return x * x; 
+        return x * x;
+    }
+
+    public static double formulaTresPuntos(double a, double b) {
+        double h = (b - a) / 2.0;
+        double x0 = a;
+        double x1 = (a + b) / 2.0;
+        double x2 = b;
+
+        double result = h * ((1.0 / 3.0) * f(x0) + (4.0 / 3.0) * f(x1) + (1.0 / 3.0) * f(x2));
+        return result;
     }
 
     
